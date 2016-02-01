45da9a45360c7e3d8834986968146bef7a0737ec
root <root@localhost.localdomain>
2016-02-01 09:11:50 -0300

    del

--- docker/nova/nova-libvirt/extend_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# TODO(SamYaple): Tweak libvirt.conf rather than change permissions.
-# Fix permissions for libvirt
-# Do not remove unless CentOS has been validated
-if [[ -c /dev/kvm ]]; then
-    chmod 660 /dev/kvm
-    chown root:kvm /dev/kvm
-fi
--- docker/zookeeper/extend_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
-    sudo chown zookeeper: /var/lib/zookeeper
-    exit 0
-fi
