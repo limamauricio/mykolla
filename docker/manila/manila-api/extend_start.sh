6193b201fba6f31390c99ee251517eb9156b82c3
root <root@localhost.localdomain>
2016-02-03 11:19:05 -0300

    update

--- docker/manila/manila-api/extend_start.sh
+++ docker/manila/manila-api/extend_start.sh
@@ -5,5 +5,7 @@ set -o errexit
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     manila-manage db sync
+    #sudo chown -R manila: /var/lib/manila/
+    #sudo chown -R manila: /var/cache/manila
     exit 0
 fi
